b6732b018b4a2c9f44d50741647a22e7da9ce0fc
Paula Soroiu <soroiu@gmail.com>
2019-05-30 16:01:25 +0200

    Modified Layout, Autentification & Membership

--- Shop/Shop.Web/Controllers/MembershipController.cs
+++ Shop/Shop.Web/Controllers/MembershipController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Shop.Web.Infrastructure;
 
 namespace Shop.Web.Controllers
 {
     public class MembershipController : Controller
     {
-        public IActionResult Index()
+
+        public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(string forename, string surename, string address, string email, int phone, string password)
+        {
+            User user = new User { Forename = forename, Surename = surename, Address = address, Email = email, Phone = phone, Password = password };
+
+            ShopRepository.Membership(user);
+
+            return RedirectToAction("Index");
+
         }
     }
 }
